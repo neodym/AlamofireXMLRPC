85a33be4c6e7c2af7fc436c9d883698067d4a7f4
Dennis Röhrs <github@dennisroehrs.de>
2021-09-15 18:16:30 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AlamofireXMLRPC",
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", majorVersion: 5),
-        .package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4)
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
+        .package(url: "https://github.com/tadija/AEXML.git", from: "4.0.0")
     ]
 )
