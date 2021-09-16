cdb58f16bb37af5c62d4d2a3f3dfb8ba7836d9a8
Dennis Röhrs <github@dennisroehrs.de>
2021-09-17 01:30:02 +0200

    order

--- Package.swift
+++ Package.swift
@@ -3,12 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "AlamofireXMLRPC",
+    products: [
+        .library(
+            name: "AlamofireXMLRPC",
+            targets: ["AlamofireXMLRPC"]
+        )
+    ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         .package(url: "https://github.com/tadija/AEXML.git", from: "4.0.0")
-    ],
-    products: [
-        .library(name: "AlamofireXMLRPC", targets: ["AlamofireXMLRPC"])
     ],
     targets: [
         .target(
